17e530e360a0360093a01a7474148c4db3c4997e
mcgrady <mcgrady911@foxmail.com>
2023-09-19 14:03:01 +0800

    feat(libs): fix build features

--- apiresponse/build.gradle.kts
+++ apiresponse/build.gradle.kts
@@ -38,6 +38,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        buildConfig = true
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
